1e1dd1b12857e816d727639f72348c8230fa39be
devtanc <devtanc@users.noreply.github.com>
2015-08-25 18:31:52 -0600

    minor fix

--- quizitive/js/login-controllers.js
+++ quizitive/js/login-controllers.js
@@ -2,7 +2,7 @@ var loginControllers = angular.module('quizitive');
 
 loginControllers.controller('loginController', function ($route, $scope, $http, store, $location) {
     $scope.login = function () {
-        window.location = 'http://in.quizitive.net/auth0-login';
+        window.location = 'http://in.quizitive.net:8080/auth0-login';
     };
 });
 
