a6eaf850cff08ce1b0dd4112e2d6231727ce10ef
devtanc <devtanc@users.noreply.github.com>
2015-10-22 22:56:27 -0600

    Skip login if valid token in storage
    If a valid auth token is found in storage, skip the login and go straight to the room-sel screen.

--- quizitive/js/login-controllers.js
+++ quizitive/js/login-controllers.js
@@ -1,49 +1,55 @@
 var loginControllers = angular.module('quizitive');
 
-loginControllers.controller('loginController', function ($scope) {
-    $scope.login = function () {
-        window.location = 'http://' + window.location.hostname + ':8080/auth0-login';
-    };
+loginControllers.controller('loginController', function ($scope, store, $location, jwtHelper) {
+		$scope.login = function () {
+				var currentToken = store.get('auth-token');
+				if (currentToken && !jwtHelper.isTokenExpired(currentToken)) {
+					//If a valid token is found, redirect to room selection page
+					$location.path('/room-sel');
+				} else {
+					window.location = 'http://' + window.location.hostname + ':8080/auth0-login';
+				}
+		};
 });
 
 loginControllers.controller('roomSelController', function ($scope, store, socket, $location, jwtHelper) {
-    console.log(store.get('auth-token'));
-    $scope.roomName = '';
-    $scope.generatedRoom = '';
+		console.log(store.get('auth-token'));
+		$scope.roomName = '';
+		$scope.generatedRoom = '';
 
-    $scope.generateRoomName = function () {
-        for (var i = 0; i < 4; i++) {
-            $scope.generatedRoom += hexGen[Math.floor((Math.random() * hexGen.length))].toUpperCase();
-        }
-        console.log("Generated Room: " + $scope.generatedRoom);
-    };
+		$scope.generateRoomName = function () {
+				for (var i = 0; i < 4; i++) {
+						$scope.generatedRoom += hexGen[Math.floor((Math.random() * hexGen.length))].toUpperCase();
+				}
+				console.log("Generated Room: " + $scope.generatedRoom);
+		};
 
-    $scope.goToRoom = function (roomName) {
-        var pathBegin = '';
-        if (jwtHelper.decodeToken(store.get('auth-token')).role == 'admin') {
-            pathBegin = '/admin/';
-        } else {
-            pathBegin = '/chat-room/';
-        }
-        if (roomName) {
-            socket.emit('joinRoom', {
-                token: store.get('auth-token'),
-                room:roomName.toLowerCase()
-            });
-            $location.path(pathBegin + roomName.toLowerCase());
-        } else {
-            socket.emit('joinRoom', {
-                token: store.get('auth-token'),
-                room:$scope.generatedRoom.toLowerCase()
-            });
-            $location.path(pathBegin + $scope.generatedRoom.toLowerCase());
-        }
-    };
+		$scope.goToRoom = function (roomName) {
+				var pathBegin = '';
+				if (jwtHelper.decodeToken(store.get('auth-token')).role == 'admin') {
+						pathBegin = '/admin/';
+				} else {
+						pathBegin = '/chat-room/';
+				}
+				if (roomName) {
+						socket.emit('joinRoom', {
+								token: store.get('auth-token'),
+								room:roomName.toLowerCase()
+						});
+						$location.path(pathBegin + roomName.toLowerCase());
+				} else {
+						socket.emit('joinRoom', {
+								token: store.get('auth-token'),
+								room:$scope.generatedRoom.toLowerCase()
+						});
+						$location.path(pathBegin + $scope.generatedRoom.toLowerCase());
+				}
+		};
 
-    var hexGen = [
-        'a', 'c', 'e', 'f', 'g', 'h', 'j', 'k', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z',
-        '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    ]; //Confusing characters removed to facilitate communication of room names
+		var hexGen = [
+				'a', 'c', 'e', 'f', 'g', 'h', 'j', 'k', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z',
+				'1', '2', '3', '4', '5', '6', '7', '8', '9'
+		]; //Confusing characters removed to facilitate communication of room names
 
-    $scope.generateRoomName();
+		$scope.generateRoomName();
 });
