82aad617c7b2be7c1c5a692f76e2c39d53af2bf3
devtanc <devtanc@users.noreply.github.com>
2015-11-02 12:19:57 -0700

    Updated port

--- js-modules/passport-init.js
+++ js-modules/passport-init.js
@@ -5,7 +5,7 @@ var strategy = new Auth0Strategy({
 		domain: 'wdd.auth0.com',
 		clientID: 'iHQKzsK9m51CfCZ8NO7COLlj9ajYxozB',
 		clientSecret: '2PTrwv59yMam6CDerMgqEZjBA7EVgKvxrJQjOHIi0Ue7zWbaCK8HOvA_3hbhD53C',
-		callbackURL: 'http://christensenhome.myds.me:8080/auth0-login-callback'
+		callbackURL: 'http://christensenhome.myds.me:40569/auth0-login-callback'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
 		return done(null, profile);
 });
