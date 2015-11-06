3e3c9da72e353db36a111ab3955de3ccf112ae92
devtanc <devtanc@users.noreply.github.com>
2015-11-06 03:01:41 -0700

    Added tests for room name validation

--- testing/todo-spec.js
+++ testing/todo-spec.js
@@ -58,4 +58,40 @@ describe('quizitive user login and room login', function() {
 			expect(browser.getCurrentUrl()).toEqual('http://in.quizitive.net/chat-room/' + room[room.length - 1].toLowerCase());
 		});
 	});
+
+	it('warns only on values that do not match the regex /^[a-zA-Z0-9-_]+$/', function() {
+		var validArray = [
+			"valid",
+			"VaLiD",
+			"VALID",
+			"valid1",
+			"123456789",
+			"qwertyuiopasdfghjklzxcvbnm1234567890",
+			"-_-",
+			"5_a-P",
+			"quizitive",
+			"last_one-to_test-HERE_123"
+		];
+		var invalidArray = [
+			"!@#$%^&*()",
+			"Room Name",
+			"what?",
+			"UPPERlower_lowerUPPER^",
+			"500%",
+			"The Big Room of Knowledge",
+			"-_-_-_-_=",
+			"this*is*not*okay",
+			"Jack's",
+			"T@lon"
+		];
+		var input = element(by.id('roomNameInput'));
+		for (var i = 0; i < validArray.length; i++) {
+			input.clear().sendKeys(validArray[i]);
+			expect(input.getAttribute('class')).toContain('ng-valid');
+		}
+		for (var i = 0; i < invalidArray.length; i++) {
+			input.clear().sendKeys(invalidArray[i]);
+			expect(input.getAttribute('class')).toContain('ng-invalid');
+		}
+	});
 });
